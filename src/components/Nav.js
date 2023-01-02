2a1e3ef346e56473ed6c03bc6eb12292d5915264
Victor-Lis <victorlisbronzo1@gmail.com>
2023-01-02 15:37:03 -0300

    v1

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,24 +3,6 @@ import './Nav-module.css';
 
 function Nav({navTitle, changePage}){
 
-  const data = {
-
-    title: 'Dev Victor Website',
-    text: 'Olha que site legal!',
-    url: 'https://react-dev-victor-landing-page.netlify.app/'
-
-  }
-
-  async function enviar(){
-
-    if(navigator.canShare && navigator.canShare(data)){
-
-      await navigator.share(data);
-
-    }
-
-  }
-
   return (
     
     <nav> 
@@ -29,7 +11,7 @@ function Nav({navTitle, changePage}){
 
         <img id="my-picture" className="nav-img" alt="My-self" src={myPicture} /> 
 
-        <h3 className="nav-title nav-title-clickabe" onClick={enviar}> Dev Victor </h3>
+        <h3 className="nav-title"> Dev Victor </h3>
 
       </div>
 
